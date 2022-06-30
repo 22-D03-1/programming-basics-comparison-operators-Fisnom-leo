7c055b6b5b52a89bb88e771ca1036fa144dcb607
Mounssif <monsifassekour@gmail.com>
2022-06-30 15:23:51 +0200

    push

---
+++ solution.js
@@ -0,0 +1,23 @@
+const apples =20;
+const oranges=30;
+console.log( apples + oranges);
+const gleissind = apples === oranges;  //aufgabe1
+console.log({gleissind});
+const nichtglei = apples !== oranges; //aufgabe 2
+console.log({nichtglei});
+const grosse = apples > oranges; //aufgabe 3
+console.log(grosse);
+const kleine = apples < oranges; //aufgabe 4
+console.log(kleine);
+const grossse =oranges> apples; //aufagbe 5
+console.log(grossse);
+const Mangos =5;
+console.log( Mangos*apples > Mangos + oranges); //aufgabe 6
+console.log(Mangos-apples<oranges+Mangos) //aufgabe 7
+const gleich = Mangos === apples === oranges;
+console.log(gleich); // aufgabe 8
+console.log( apples /Mangos === oranges / Mangos ); // aufgabe 9
+console.log(Mangos+apples > oranges - Mangos); // aufagabe 10A
+
+const ergib11 = Mangos+apples>=oranges-Mangos
+console.log({ergib11});
